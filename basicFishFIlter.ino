ff8dd35ec9e09b4cd349ad4c177ab62b0d10d160
thesimplekid <thesimplekid>
2020-03-29 16:03:42 -0400

    delay working

--- basicFishFIlter.ino
+++ basicFishFIlter.ino
@@ -2,6 +2,13 @@
 int led = 4;
 int button = 5;
 
+unsigned long buttonPushedMillis;
+unsigned long ledTurnedOnAt;
+unsigned long turnOffDelay = 5000;
+unsigned long currentMillis;
+//unsigned long turnOnDelay = 0;
+
+//boolean ledReady = false;
 
 
 int ledState = HIGH;
@@ -20,14 +27,50 @@ void setup() {
 
 }
 
+void filterOff(){
+  //if ((unsigned long)(currentMillis - buttonPushedMillis) >= turnOnDelay){
+      digitalWrite(led, HIGH);
+     // setup our next "state"
+     ledState = true;
+     // save when the LED turned on
+     ledTurnedOnAt = currentMillis;
+     // wait for next button press
+//     ledReady = false;
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
+  currentMillis = millis();
 
   buttonCurrent = digitalRead(button);
 
+  if (buttonCurrent == HIGH){
+    buttonPushedMillis = currentMillis;
+    //ledReady = true;
+    filterOff();
+  }
+/*
+  if (ledReady){
+    //if ((unsigned long)(currentMillis - buttonPushedMillis) >= turnOnDelay){
+      digitalWrite(led, HIGH);
+     // setup our next "state"
+     ledState = true;
+     // save when the LED turned on
+     ledTurnedOnAt = currentMillis;
+     // wait for next button press
+     ledReady = false;
+   // }
+  }
+*/
+  if (ledState) {
+   // okay, led on, check for now long
+   if ((unsigned long)(currentMillis - ledTurnedOnAt) >= turnOffDelay) {
+     ledState = false;
+     digitalWrite(led, LOW);
+   }
+ }
+/*
   if (buttonCurrent == HIGH && buttonPrev == LOW && millis() - timeer > debounce){
-    
-    Serial.print(ledState);
     if (ledState == HIGH){
       ledState = LOW;
       Serial.print("low");
@@ -42,6 +85,6 @@ void loop() {
   //Serial.print("Here");
   //Serial.print(buttonCurrent);
   buttonPrev = buttonCurrent;
-  
+  */
 
 }
